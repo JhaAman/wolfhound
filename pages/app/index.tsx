c6ba9d096e0246c76d1a2d02b0858c5c017fc5de
Aman Jha <gunujha@gmail.com>
2021-12-19 11:12:58 -0800

    starting to style the main app - im really bad at css though

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -5,7 +5,23 @@ import AppLayout from "../../layout/AppLayout";
 interface Props {}
 
 const MainApp: Page = () => {
-  return <div className="text-black dark:text-white">Hello World</div>;
+  return (
+    <div className="text-black dark:text-white">
+      <div className="flex flex-row items-center justify-center">
+        {/* Question Panel */}
+        <div className="w-full h-screen max-w-xl mx-12 mt-20 mb-20 text-center bg-gray-500 max-h-72 ">
+          Question
+        </div>
+        <div className="w-full max-w-xl mx-12 mt-20 mb-20 text-center bg-gray-400 h-96">
+          Answer
+        </div>
+
+        {/* Answer Panel */}
+      </div>
+
+      {/* Dock */}
+    </div>
+  );
 };
 
 // Attach the landing layout (and other nested layouts) to the page
