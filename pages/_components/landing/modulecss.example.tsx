61dc77c65bbaa88cde07d1d6a66840106be2d125
Aman Jha <gunujha@gmail.com>
2021-12-20 13:29:44 -0800

    another vercel issue that my local thing didnt care about

--- pages/_components/landing/modulecss.example.tsx
+++ pages/_components/landing/modulecss.example.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../../../styles/Home.module.css";
 
 interface Props {}
 
