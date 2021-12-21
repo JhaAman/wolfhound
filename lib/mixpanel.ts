d8e94fc198036386adfcbbbd427b61e71885f329
Aman Jha <gunujha@gmail.com>
2021-12-21 14:02:41 -0800

    added mixpanel

---
+++ lib/mixpanel.ts
@@ -0,0 +1,10 @@
+import mixpanel from "mixpanel-browser";
+// or with require() syntax:
+// const mixpanel = require('mixpanel-browser');
+
+// Enabling the debug mode flag is useful during implementation,
+// but it's recommended you remove it for production
+mixpanel.init("c65635ca1d6461267733659d2c821911", {
+  debug: process.env.NODE_ENV === "development",
+});
+mixpanel.track("Sign up");
