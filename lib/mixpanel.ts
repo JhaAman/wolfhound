4291c990c4eeb6cc9eea5c97a2d3ce1b1f2a1952
Aman Jha <gunujha@gmail.com>
2021-12-24 13:16:42 -0800

    more lib comments

--- lib/mixpanel.ts
+++ lib/mixpanel.ts
@@ -5,11 +5,6 @@
  */
 
 import mixpanel from "mixpanel-browser";
-// or with require() syntax:
-// const mixpanel = require('mixpanel-browser');
-
-// Enabling the debug mode flag is useful during implementation,
-// but it's recommended you remove it for production
 
 export function mixpanelInit() {
   mixpanel.init("c65635ca1d6461267733659d2c821911", {
--- lib/supabase.ts
+++ lib/supabase.ts
@@ -6,6 +6,8 @@
 
 import { createClient } from "@supabase/supabase-js";
 
+// These env variables are different for production and development
+// Found in .env.development and .env.production
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
