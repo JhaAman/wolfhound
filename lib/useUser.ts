2f74a538022b070a903891d937a42b5915e75c08
Aman Jha <gunujha@gmail.com>
2021-12-25 14:58:44 -0800

    gonna try to use api routes

--- lib/useUser.ts
+++ lib/useUser.ts
@@ -1,120 +1,33 @@
-import { useEffect, useState, createContext, useContext } from "react";
-import { Session, User, Provider } from "@supabase/supabase-js";
-import supabase from "@/lib/supabase";
-import { UserDetails, Subscription } from "@/lib/types";
+import { useEffect } from "react";
+import Router from "next/router";
+import useSWR from "swr";
 
-type UserContextType = {
-  session: Session;
-  user: User;
-  userDetails: UserDetails;
-  userLoaded: boolean;
-  subscription: Subscription;
-  signIn: (options: SignInOptions) => Promise<{
-    session: Session | null;
-    user: User | null;
-    provider?: Provider;
-    url?: string | null;
-    error: Error | null;
-    data: Session | null;
-  }>;
-  signUp: (options: SignUpOptions) => Promise<{
-    user: User | null;
-    session: Session | null;
-    error: Error | null;
-    data: Session | User | null;
-  }>;
-  signOut: () => void;
+export type User = {
+  isLoggedIn: boolean;
+  login: string;
+  avatarUrl: string;
 };
 
-export const UserContext = createContext<UserContextType | undefined>(
-  undefined
-);
-
-export const UserContextProvider = (props: any) => {
-  const [userLoaded, setUserLoaded] = useState(false);
-  const [session, setSession] = useState<Session | null>(null);
-  const [user, setUser] = useState<User | null>(null);
-  const [userDetails, setUserDetails] = useState<UserDetails | null>(null);
-  const [subscription, setSubscription] = useState<Subscription | null>(null);
+export default function useUser({
+  redirectTo = "",
+  redirectIfFound = false,
+} = {}) {
+  const { data: user, mutate: mutateUser } = useSWR<User>("/api/user");
 
   useEffect(() => {
-    const session = supabase.auth.session();
-    setSession(session);
-    setUser(session?.user ?? null);
-    const { data: authListener } = supabase.auth.onAuthStateChange(
-      async (event, session) => {
-        setSession(session);
-        setUser(session?.user ?? null);
-      }
-    );
+    // if no redirect needed, just return (example: already on /dashboard)
+    // if user data not yet there (fetch in progress, logged in or not) then don't do anything yet
+    if (!redirectTo || !user) return;
 
-    return () => {
-      authListener?.unsubscribe();
-    };
-  }, []);
-
-  const getUserDetails = () =>
-    supabase.from<UserDetails>("users").select("*").single();
-  const getSubscription = () =>
-    supabase
-      .from<Subscription>("subscriptions")
-      .select("*, prices(*, products(*))")
-      .in("status", ["trialing", "active"])
-      .single();
-
-  useEffect(() => {
-    if (user) {
-      Promise.allSettled([getUserDetails(), getSubscription()]).then(
-        (results) => {
-          const userDetailsPromise = results[0];
-          const subscriptionPromise = results[1];
-
-          console.log("subscription", subscriptionPromise);
-          if (userDetailsPromise.status === "fulfilled")
-            setUserDetails(userDetailsPromise.value.data);
-
-          if (subscriptionPromise.status === "fulfilled")
-            setSubscription(subscriptionPromise.value.data);
-
-          setUserLoaded(true);
-        }
-      );
+    if (
+      // If redirectTo is set, redirect if the user was not found.
+      (redirectTo && !redirectIfFound && !user?.isLoggedIn) ||
+      // If redirectIfFound is also set, redirect if the user was found
+      (redirectIfFound && user?.isLoggedIn)
+    ) {
+      Router.push(redirectTo);
     }
-  }, [user]);
+  }, [user, redirectIfFound, redirectTo]);
 
-  const value = {
-    session,
-    user,
-    userDetails,
-    userLoaded,
-    subscription,
-    signIn: (options: SignInOptions) => supabase.auth.signIn(options),
-    signUp: (options: SignUpOptions) => supabase.auth.signUp(options),
-    signOut: () => {
-      setUserDetails(null);
-      setSubscription(null);
-      return supabase.auth.signOut();
-    },
-  };
-
-  return <UserContext.Provider value={value} {...props} />;
-};
-
-export const useUser = () => {
-  const context = useContext(UserContext);
-  if (context === undefined) {
-    throw new Error(`useUser must be used within a UserContextProvider.`);
-  }
-  return context;
-};
-
-type SignInOptions = {
-  email?: string;
-  password?: string;
-  provider?: Provider;
-};
-
-type SignUpOptions = {
-  email: string;
-  password: string;
-};
+  return { user, mutateUser };
+}
