93d2446b4903262e4a976c305dc8906c1270a7da
Aman Jha <gunujha@gmail.com>
2021-12-25 14:37:08 -0800

    trying to use the static method, which has faster first render

--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -1,26 +1,32 @@
-import Link from 'next/link';
-import s from './Navbar.module.css';
+import Link from "next/link";
+import s from "./Navbar.module.css";
 
-import Logo from 'components/icons/Logo';
-import { useUser } from 'utils/useUser';
+import Logo from "../../icons/Logo";
+import { useRouter } from "next/router";
+import supabase from "../../../lib/supabase";
 
 const Navbar = () => {
-  const { user, signOut } = useUser();
+  const router = useRouter();
+
+  async function signOut() {
+    await supabase.auth.signOut();
+    router.push("/signin");
+  }
 
   return (
     <nav className={s.root}>
       <a href="#skip" className="sr-only focus:not-sr-only">
         Skip to content
       </a>
-      <div className="mx-auto max-w-6xl px-6">
-        <div className="flex justify-between align-center flex-row py-4 md:py-6 relative">
-          <div className="flex flex-1 items-center">
+      <div className="max-w-6xl px-6 mx-auto">
+        <div className="relative flex flex-row justify-between py-4 align-center md:py-6">
+          <div className="flex items-center flex-1">
             <Link href="/">
               <a className={s.logo} aria-label="Logo">
                 <Logo />
               </a>
             </Link>
-            <nav className="space-x-2 ml-6 hidden lg:block">
+            <nav className="hidden ml-6 space-x-2 lg:block">
               <Link href="/">
                 <a className={s.link}>Pricing</a>
               </Link>
@@ -30,7 +36,7 @@ const Navbar = () => {
             </nav>
           </div>
 
-          <div className="flex flex-1 justify-end space-x-8">
+          <div className="flex justify-end flex-1 space-x-8">
             {user ? (
               <Link href="#">
                 <a className={s.link} onClick={() => signOut()}>
--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,7 +1,7 @@
 import { User } from "@supabase/supabase-js";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useState, FormEvent, Provider, ReactElement } from "react";
+import { useState, FormEvent, ReactElement } from "react";
 import GitHub from "../components/icons/GitHub";
 import Logo from "../components/icons/Logo";
 import Meta from "../components/landing/Meta";
@@ -16,17 +16,16 @@ interface Props {
   beta_list: any;
 }
 
+type Provider = "github";
+
 const SignIn = ({ user, beta_list }: Props) => {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [showPasswordInput, setShowPasswordInput] = useState(false);
   const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState<{ type?: string; content?: string }>({
     type: "",
     content: "",
   });
   const router = useRouter();
-  const { signIn } = useUser();
 
   const handleSignin = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -34,22 +33,22 @@ const SignIn = ({ user, beta_list }: Props) => {
     setLoading(true);
     setMessage({});
 
-    const { error } = await signIn({ email, password });
+    const { error } = await supabase.auth.signIn({ email });
     if (error) {
       setMessage({ type: "error", content: error.message });
     }
-    if (!password) {
-      setMessage({
-        type: "note",
-        content: "Check your email for the magic link.",
-      });
-    }
+
+    setMessage({
+      type: "note",
+      content: "Check your email for the magic link.",
+    });
+
     setLoading(false);
   };
 
   const handleOAuthSignIn = async (provider: Provider) => {
     setLoading(true);
-    const { error } = await signIn({ provider });
+    const { error } = await supabase.auth.signIn({ provider });
     if (error) {
       setMessage({ type: "error", content: error.message });
     }
@@ -57,12 +56,15 @@ const SignIn = ({ user, beta_list }: Props) => {
   };
 
   if (!user)
+    // user shouldn't even be here if they're logged in
     return (
       <div className="flex justify-center height-screen-helper">
         <div className="flex flex-col justify-between max-w-lg p-3 m-auto w-80 ">
           <div className="flex justify-center pb-12 ">
             <Logo width="64px" height="64px" />
           </div>
+
+          {/* Display messages and errors */}
           <div className="flex flex-col space-y-4">
             {message.content && (
               <div
@@ -78,79 +80,23 @@ const SignIn = ({ user, beta_list }: Props) => {
               </div>
             )}
 
-            {!showPasswordInput && (
-              <form onSubmit={handleSignin} className="flex flex-col space-y-4">
-                <Input
-                  type="email"
-                  placeholder="Email"
-                  value={email}
-                  onChange={setEmail}
-                  required
-                />
-                <Button
-                  variant="slim"
-                  type="submit"
-                  loading={loading}
-                  disabled={!email.length}
-                >
-                  Send magic link
-                </Button>
-              </form>
-            )}
-
-            {showPasswordInput && (
-              <form onSubmit={handleSignin} className="flex flex-col space-y-4">
-                <Input
-                  type="email"
-                  placeholder="Email"
-                  value={email}
-                  onChange={setEmail}
-                  required
-                />
-                <Input
-                  type="password"
-                  placeholder="Password"
-                  value={password}
-                  onChange={setPassword}
-                  required
-                />
-                <Button
-                  className="mt-1"
-                  variant="slim"
-                  type="submit"
-                  loading={loading}
-                  disabled={!password.length || !email.length}
-                >
-                  Sign in
-                </Button>
-              </form>
-            )}
-
-            <span className="pt-1 text-sm text-center">
-              <a
-                href="#"
-                className="text-gray-200 cursor-pointer text-accent-9 hover:underline"
-                onClick={() => {
-                  if (showPasswordInput) setPassword("");
-                  setShowPasswordInput(!showPasswordInput);
-                  setMessage({});
-                }}
+            <form onSubmit={handleSignin} className="flex flex-col space-y-4">
+              <Input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={setEmail}
+                required
+              />
+              <Button
+                variant="slim"
+                type="submit"
+                loading={loading}
+                disabled={!email.length}
               >
-                {`Or sign in with ${
-                  showPasswordInput ? "magic link" : "password"
-                }.`}
-              </a>
-            </span>
-
-            <span className="pt-1 text-sm text-center">
-              <span className="text-gray-200">Don't have an account?</span>
-              {` `}
-              <Link href="/signup">
-                <a className="font-bold cursor-pointer text-accent-9 hover:underline">
-                  Sign up.
-                </a>
-              </Link>
-            </span>
+                Send magic link
+              </Button>
+            </form>
           </div>
 
           <div className="flex items-center my-6">
@@ -197,8 +143,6 @@ export async function getServerSideProps(ctx: { req: any }) {
 
   // If we don't have a user logged in, then continue sign in process
   const { data, error } = await supabase.from("Beta List").select();
-
-  /* if a user is set, pass it to the page via props */
   return { props: { beta_list: data } };
 }
 
