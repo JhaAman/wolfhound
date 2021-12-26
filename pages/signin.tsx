98fd46bcafdceac7dc449ce847177a7c38ce9f53
Aman Jha <gunujha@gmail.com>
2021-12-25 16:17:39 -0800

    not sure what the input ssr issue is - have to see

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -66,7 +66,7 @@ const SignIn = ({}: Props) => {
 
   // user shouldn't even be here if they're logged in
   return (
-    <div className="flex justify-center height-screen-helper">
+    <div className="flex justify-center h-screen">
       <div className="flex flex-col justify-between max-w-lg p-3 m-auto w-80 ">
         <div className="flex justify-center pb-12 ">
           <Logo width="64px" height="64px" />
