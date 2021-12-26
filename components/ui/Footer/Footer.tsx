b926147f15b0be836b052ae82d4754d0e886986d
Aman Jha <gunujha@gmail.com>
2021-12-25 16:35:15 -0800

    should be ready to test on vercel

--- components/ui/Footer/Footer.tsx
+++ components/ui/Footer/Footer.tsx
@@ -1,17 +1,17 @@
-import Link from 'next/link';
-import s from './Footer.module.css';
+import Link from "next/link";
+import s from "./Footer.module.css";
 
-import Logo from 'components/icons/Logo';
-import GitHub from 'components/icons/GitHub';
+import Logo from "components/icons/Logo";
+import GitHub from "components/icons/GitHub";
 
 export default function Footer() {
   return (
     <footer className="mx-auto max-w-[1920px] px-6 bg-gray-900">
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 border-b border-gray-600 py-12 text-white transition-colors duration-150 bg-gray-900">
+      <div className="grid grid-cols-1 gap-8 py-12 text-white transition-colors duration-150 bg-gray-900 border-b border-gray-600 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-2">
           <Link href="/">
-            <a className="flex flex-initial items-center font-bold md:mr-24">
-              <span className="rounded-full border border-gray-700 mr-2">
+            <a className="flex items-center flex-initial font-bold md:mr-24">
+              <span className="mr-2 border border-gray-700 rounded-full">
                 <Logo />
               </span>
               <span>ACME</span>
@@ -19,31 +19,31 @@ export default function Footer() {
           </Link>
         </div>
         <div className="col-span-1 lg:col-span-2">
-          <ul className="flex flex-initial flex-col md:flex-1">
+          <ul className="flex flex-col flex-initial md:flex-1">
             <li className="py-3 md:py-0 md:pb-4">
               <Link href="/">
-                <a className="text-white hover:text-gray-200 transition ease-in-out duration-150">
+                <a className="text-white transition duration-150 ease-in-out hover:text-gray-200">
                   Home
                 </a>
               </Link>
             </li>
             <li className="py-3 md:py-0 md:pb-4">
               <Link href="/">
-                <a className="text-white hover:text-gray-200 transition ease-in-out duration-150">
+                <a className="text-white transition duration-150 ease-in-out hover:text-gray-200">
                   About
                 </a>
               </Link>
             </li>
             <li className="py-3 md:py-0 md:pb-4">
               <Link href="/">
-                <a className="text-white hover:text-gray-200 transition ease-in-out duration-150">
+                <a className="text-white transition duration-150 ease-in-out hover:text-gray-200">
                   Careers
                 </a>
               </Link>
             </li>
             <li className="py-3 md:py-0 md:pb-4">
               <Link href="/">
-                <a className="text-white hover:text-gray-200 transition ease-in-out duration-150">
+                <a className="text-white transition duration-150 ease-in-out hover:text-gray-200">
                   Blog
                 </a>
               </Link>
@@ -51,30 +51,30 @@ export default function Footer() {
           </ul>
         </div>
         <div className="col-span-1 lg:col-span-2">
-          <ul className="flex flex-initial flex-col md:flex-1">
+          <ul className="flex flex-col flex-initial md:flex-1">
             <li className="py-3 md:py-0 md:pb-4">
-              <p className="text-white font-bold hover:text-gray-200 transition ease-in-out duration-150">
+              <p className="font-bold text-white transition duration-150 ease-in-out hover:text-gray-200">
                 LEGAL
               </p>
             </li>
             <li className="py-3 md:py-0 md:pb-4">
               <Link href="/">
-                <a className="text-white hover:text-gray-200 transition ease-in-out duration-150">
+                <a className="text-white transition duration-150 ease-in-out hover:text-gray-200">
                   Privacy Policy
                 </a>
               </Link>
             </li>
             <li className="py-3 md:py-0 md:pb-4">
               <Link href="/">
-                <a className="text-white hover:text-gray-200 transition ease-in-out duration-150">
+                <a className="text-white transition duration-150 ease-in-out hover:text-gray-200">
                   Terms of Use
                 </a>
               </Link>
             </li>
           </ul>
         </div>
-        <div className="col-span-1 lg:col-span-6 flex items-start lg:justify-end text-white">
-          <div className="flex space-x-6 items-center h-10">
+        <div className="flex items-start col-span-1 text-white lg:col-span-6 lg:justify-end">
+          <div className="flex items-center h-10 space-x-6">
             <a
               aria-label="Github Repository"
               href="https://github.com/vercel/nextjs-subscription-payments"
@@ -84,7 +84,7 @@ export default function Footer() {
           </div>
         </div>
       </div>
-      <div className="py-12 flex flex-col md:flex-row justify-between items-center space-y-4 bg-gray-900">
+      <div className="flex flex-col items-center justify-between py-12 space-y-4 bg-gray-900 md:flex-row">
         <div>
           <span>&copy; 2020 ACME, Inc. All rights reserved.</span>
         </div>
